ea5531e70fb1649cd4b2635562842f16af956041
mobcoder-manish-khulbe <mobcoder-manish-khulbe>
2023-04-18 18:18:56 +0530

    new test cases

--- tickets/src/routes/__test__/new.test.ts
+++ tickets/src/routes/__test__/new.test.ts
@@ -12,6 +12,12 @@ it("return a status other than 401 if the user is signed in", async () => {
     const response = await request(app).post('/api/tickets').set('Cookie', global.signin()).send({})
     expect(response.status).not.toEqual(401)
 });
-// it("return an error if an invalid title is provided", async () => {});
-// it("return an error if an invalid price is provided", async () => {});
+it("return an error if an invalid title is provided", async () => {
+    await request(app).post('/api/tickets').set('Cookie', global.signin()).send({  title: '',   price: 10,  }).expect(400)
+    await request(app).post('/api/tickets').set('Cookie', global.signin()).send({    price: 10,  }).expect(400)
+});
+it("return an error if an invalid price is provided", async () => {
+    await request(app).post('/api/tickets').set('Cookie', global.signin()).send({  title: 'new',   price: -10,  }).expect(400)
+    await request(app).post('/api/tickets').set('Cookie', global.signin()).send({   title: 'new'  }).expect(400)
+});
 // it("creates a ticket with valid inputs ", async () => {});

4b20988a652da8c9dcc03375599c79418c9aecd1
manishkhulbe <manishkhulbe123@gmail.com>
2023-05-12 15:37:51 +0530

    event folder in common

--- nats-test/src/publisher.ts
+++ nats-test/src/publisher.ts
@@ -1,19 +1,21 @@
+import { TicketCreatedPublisher } from './events/ticket-created-publisher';
 import nats from 'node-nats-streaming'
 
 const stan= nats.connect('ticketing', 'abc',{
     url:'http://localhost:4222'
 })
 
-stan.on('connect',()=>{
+stan.on('connect',async()=>{
     console.log('Publisher connected to NATS')
 
-    const data= JSON.stringify({
-        id:'234',
-        title:'conser',
-        price:33
-    })
-
-    stan.publish('ticket:created', data,()=>{
-        console.log('event published')
-    })
+   const publisher = new TicketCreatedPublisher(stan)
+   try {
+       await publisher.publish({
+        id: '123',
+        title:'concert',
+        price: 20
+       })
+   } catch (error) {
+    console.error(error)
+   }
 })
